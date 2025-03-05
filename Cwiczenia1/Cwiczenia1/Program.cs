fd236dc9516eabf319503275cfb6137cd8c7405e
Oleh Korsun <s30911@pjwstk.edu.pl>
2025-03-05 16:51:09 +0100

    Commands

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!2");
 Console.WriteLine("Hello, World!3");
 Console.WriteLine("Bardzo ciekawy przedmiot, poznajmy go jeszcze głębiej!");
+
+// git status
+// git add --all
+// git commit -m ""
+// git push
