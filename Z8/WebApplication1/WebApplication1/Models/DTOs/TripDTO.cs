f54a972c62c7c0fe2ae99d1c1bd2dc62ef868f8a
Oleh Korsun <oleg.korsun.0505@gmail.com>
2025-04-30 15:04:12 +0200

    +

--- Z8/WebApplication1/WebApplication1/Models/DTOs/TripDTO.cs
+++ Z8/WebApplication1/WebApplication1/Models/DTOs/TripDTO.cs
@@ -4,10 +4,10 @@ public class TripDTO
 {
     public int IdTrip {get; set;}
     public string Name {get; set;}
-    public List<CountryDTO> Countries { get; set; }
     public DateTime DateFrom {get; set;}
     public DateTime DateTo {get; set;}
     public int MaxPeople {get; set;}
+    public List<CountryDTO> Countries { get; set; }
 }
 
 
