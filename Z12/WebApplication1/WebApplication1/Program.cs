afad46f3ab8417f21b381317b12cbd562b0c6c43
Oleh Korsun <oleg.korsun.0505@gmail.com>
2025-05-26 23:09:05 +0200

    Dodalem wstrzykiwanie zaleznosci oraz stworzylem klase ClientService

--- Z12/WebApplication1/WebApplication1/Program.cs
+++ Z12/WebApplication1/WebApplication1/Program.cs
@@ -1,10 +1,31 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    // Żeby uniknąć pętli w zależnościach pomiędzy tabelami
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    options.JsonSerializerOptions.WriteIndented = true;
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApbdContext>(opt =>
+{
+    opt.UseSqlServer(connectionString);
+});
+
+builder.Services.AddScoped<ITripService, TripService>();
+builder.Services.AddScoped<IClientService, ClientService>();
 
 var app = builder.Build();
 
